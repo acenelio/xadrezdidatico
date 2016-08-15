af4d28b328b02b23fe1e75c9a6ee6d7f7fd9f1e2
Nelio Alves <nelio@gmail.com>
2016-08-15 00:14:54 -0300

    Primeiro commit

---
+++ Assets/Scripts/PrologoUIController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PrologoUIController : MonoBehaviour {
+
+    public void Sair() {
+        SceneManager.LoadScene("inicio");
+    }
+}
